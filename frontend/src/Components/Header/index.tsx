91bccfa226b39666abe322a31e42ff7c4bfb5b12
Alexsandro Costa <alexs.alexsandrocosta@hotmail.com>
2022-10-03 15:33:18 -0300

    Other Components

---
+++ frontend/src/Components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logo from "../../assets/img/logo.jpg"
+import './styles.css'
+
+function Header() {
+    return (
+
+        <header>
+        <div className ="dsmeta-logo-container">
+            <img src= {logo} alt="DSMeta" />
+            <h1>AAC-Security</h1>
+            <p>Desenvolvido por
+                <a href="https://www.instagram.com/alexs.alexsandrocosta/"> @AAC-Security</a>
+            </p>
+        </div>
+    </header>
+
+
+    )
+}
+export default Header
